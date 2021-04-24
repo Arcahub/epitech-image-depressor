c2ae7a09c25e2ebc3ffe40c542abb78ba631507d
Arcarox <arcarox@localhost.localdomain>
2021-04-24 19:28:17 +0200

    Update help args name

--- src/opts.rs
+++ src/opts.rs
@@ -5,9 +5,9 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "basic")]
 pub struct Opts {
-    #[structopt(short, long, parse(from_os_str))]
+    #[structopt(name = "input_path", short, long, parse(from_os_str))]
     pub input: PathBuf,
 
-    #[structopt(short, long, parse(from_os_str))]
+    #[structopt(name = "output_path", short, long, parse(from_os_str))]
     pub output: PathBuf,
 }
